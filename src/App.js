c16d9cab613257c5f098a24ba0cdf908af989fd1
Kishan Somaiya <kishansomaiya271@gmail.com>
2021-03-10 22:11:13 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Home from "./Home";
+import Footer from './Footer';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SearchPage from './SearchPage';
+
+function App() {
+  return (
+
+    //BEM convention
+    <div className='app'>
+      
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path='/search'>
+            <SearchPage />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+
+        <Footer />
+
+      </Router>
+      
+    </div>
+  )
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDOSSFaHQAiBjyaZTBuZUaDDejzWSsvYEg",
+    authDomain: "airbnb-clonelight.firebaseapp.com",
+    databaseURL: "https://airbnb-clonelight.firebaseio.com",
+    projectId: "airbnb-clonelight",
+    storageBucket: "airbnb-clonelight.appspot.com",
+    messagingSenderId: "170801937060",
+    appId: "1:170801937060:web:e1dac7a36a561d3dba0431",
+    measurementId: "G-1ZGK8VDF5R"
+  };
+
+  //https://airbnb-clonelight.web.app
